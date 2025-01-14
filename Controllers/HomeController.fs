7372baf5f4d952438e6aa1b9c000ca2fbb410979
Isaac Schemm <isaacschemm@gmail.com>
2025-01-13 20:59:20 -0600

    Remove extraneous files from template

--- Controllers/HomeController.fs
+++ Controllers/HomeController.fs
@@ -9,23 +9,8 @@ open System.Diagnostics
 open Microsoft.AspNetCore.Mvc
 open Microsoft.Extensions.Logging
 
-open SatRadioProxy.Models
-
-type HomeController (logger : ILogger<HomeController>) =
+type HomeController () =
     inherit Controller()
 
     member this.Index () =
         this.View()
-
-    member this.Privacy () =
-        this.View()
-
-    [<ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)>]
-    member this.Error () =
-        let reqId = 
-            if isNull Activity.Current then
-                this.HttpContext.TraceIdentifier
-            else
-                Activity.Current.Id
-
-        this.View({ RequestId = reqId })

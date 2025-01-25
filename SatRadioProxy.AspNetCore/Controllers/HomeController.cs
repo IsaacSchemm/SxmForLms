e19e1a572e543f97e93d3429c4f4ddc87f1895f5
Isaac Schemm <isaacschemm@gmail.com>
2025-01-24 22:13:13 -0600

    Remove bookmark functionality

--- SatRadioProxy.AspNetCore/Controllers/HomeController.cs
+++ SatRadioProxy.AspNetCore/Controllers/HomeController.cs
@@ -1,6 +1,5 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using SatRadioProxy.SiriusXM;
-using System.Threading;
 
 namespace SatRadioProxy.Web.Controllers
 {
@@ -16,43 +15,19 @@ namespace SatRadioProxy.Web.Controllers
             return View();
         }
 
-        [HttpPost]
-        public IActionResult SetBookmarks(string[] id)
-        {
-            BookmarkManager.setBookmarks(id);
-            return RedirectToAction(nameof(Index));
-        }
-
         public async Task<IActionResult> PlayChannel(int num, CancellationToken cancellationToken)
         {
-            string ipAddress = NetworkInterfaceProvider.address;
+            var address = NetworkInterfaceProvider.address;
+            var port = NetworkInterfaceProvider.port;
 
             var channels = await SiriusXMChannelCache.getChannelsAsync(cancellationToken);
 
             var channel = channels
                 .Where(c => c.channelNumber == $"{num}")
                 .FirstOrDefault();
-            return channel != null
-                ? Redirect($"http://{ipAddress}:5000/Proxy/playlist-{channel.channelId}.m3u8")
-                : NotFound();
-        }
-
-        public async Task<IActionResult> PlayBookmark(int num, CancellationToken cancellationToken)
-        {
-            string ipAddress = NetworkInterfaceProvider.address;
-
-            var bookmarks = BookmarkManager.getBookmarks();
-
-            var channels = await SiriusXMChannelCache.getChannelsAsync(cancellationToken);
-
-            var channel = channels
-                .OrderBy(c => bookmarks.Contains(c.channelId) ? 1 : 2)
-                .ThenBy(c => int.TryParse(c.channelNumber, out int n) ? n : 0)
-                .Skip(num - 1)
-                .FirstOrDefault();
 
             return channel != null
-                ? Redirect($"http://{ipAddress}:5000/Proxy/playlist-{channel.channelId}.m3u8")
+                ? Redirect($"http://{address}:{port}/Proxy/playlist-{channel.channelId}.m3u8")
                 : NotFound();
         }
     }

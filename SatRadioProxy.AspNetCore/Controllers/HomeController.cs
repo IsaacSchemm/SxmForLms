f1e3ce9a4a0603fcd33a6348937fe90885fc3377
Isaac Schemm <isaacschemm@gmail.com>
2025-01-24 22:21:15 -0600

    Use relative redirect URL now that proxy is on the same port

--- SatRadioProxy.AspNetCore/Controllers/HomeController.cs
+++ SatRadioProxy.AspNetCore/Controllers/HomeController.cs
@@ -1,5 +1,7 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using SatRadioProxy.SiriusXM;
+using System.Text;
+using System.Text.Json;
 
 namespace SatRadioProxy.Web.Controllers
 {
@@ -15,11 +17,32 @@ namespace SatRadioProxy.Web.Controllers
             return View();
         }
 
+        public async Task<IActionResult> ChannelInfo(CancellationToken cancellationToken)
+        {
+            var channels = await SiriusXMChannelCache.getChannelsAsync(cancellationToken);
+
+            var channelInfo = channels.Select(c => new
+            {
+                c.channelNumber,
+                c.name,
+                c.mediumDescription,
+                image = c.images.images
+                        .Where(i => i.name == "color channel logo (on dark)")
+                        .Where(i => i.width * 1.0 / i.height == 1.25)
+                        .Select(i => i.url)
+                        .FirstOrDefault()
+            });
+
+            string json = JsonSerializer.Serialize(channelInfo);
+
+            return Content(
+                $"var channelInfo = {json};",
+                "text/javascript",
+                Encoding.UTF8);
+        }
+
         public async Task<IActionResult> PlayChannel(int num, CancellationToken cancellationToken)
         {
-            var address = NetworkInterfaceProvider.address;
-            var port = NetworkInterfaceProvider.port;
-
             var channels = await SiriusXMChannelCache.getChannelsAsync(cancellationToken);
 
             var channel = channels
@@ -27,7 +50,7 @@ namespace SatRadioProxy.Web.Controllers
                 .FirstOrDefault();
 
             return channel != null
-                ? Redirect($"http://{address}:{port}/Proxy/playlist-{channel.channelId}.m3u8")
+                ? Redirect($"/Proxy/playlist-{channel.channelId}.m3u8")
                 : NotFound();
         }
     }

524f5bf8bef1f6706ba24621225f38951b74662a
Isaac Schemm <isaacschemm@gmail.com>
2025-05-04 14:34:42 -0500

    Show display contents in iframe after performing action on player

--- RadioHomeEngine.AspNetCore/Controllers/PlayerController.cs
+++ RadioHomeEngine.AspNetCore/Controllers/PlayerController.cs
@@ -1,11 +1,13 @@
 ﻿using Microsoft.AspNetCore.Mvc;
-
+using System.Net;
 using static RadioHomeEngine.LyrionCLI;
 
 namespace RadioHomeEngine.AspNetCore.Controllers
 {
     public class PlayerController : Controller
     {
+        private const string AUTOHIDE = "<script type='text/javascript'>setTimeout(function(){location.href='about:blank';},3000)</script>";
+
         public IActionResult Index(string id)
         {
             var player = Player.NewPlayer(id);
@@ -13,60 +15,75 @@ namespace RadioHomeEngine.AspNetCore.Controllers
         }
 
         [HttpPost]
-        public async Task PlaySiriusXMChannel(string id, int number)
+        public async Task<IActionResult> PlaySiriusXMChannel(string id, int number)
         {
             await AtomicActions.performActionAsync(
                 Player.NewPlayer(id),
                 AtomicAction.NewPlaySiriusXMChannel(number));
+
+            return await Display(id);
         }
 
         [HttpPost]
-        public async Task PlayBrownNoise(string id)
+        public async Task<IActionResult> PlayBrownNoise(string id)
         {
             await AtomicActions.performActionAsync(
                 Player.NewPlayer(id),
                 AtomicAction.PlayBrownNoise);
+
+            return await Display(id);
         }
 
         [HttpPost]
-        public async Task StreamInfo(string id)
+        public async Task<IActionResult> StreamInfo(string id)
         {
             await AtomicActions.performActionAsync(
                 Player.NewPlayer(id),
                 AtomicAction.StreamInfo);
 
-            //var p1 = await Players.getDisplayAsync(
-            //    Player.NewPlayer(id));
-
-            //var p2 = await Players.getDisplayAsync(
-            //    Player.NewPlayer(id));
-
-            //Console.WriteLine(p1);
-            //Console.WriteLine(p2);
+            return await Display(id);
         }
 
         [HttpPost]
-        public async Task PlayTrack(string id, int number)
+        public async Task<IActionResult> PlayTrack(string id, int number)
         {
             await AtomicActions.performActionAsync(
                 Player.NewPlayer(id),
                 AtomicAction.NewPlayTrack(number));
+
+            return await Display(id);
         }
 
         [HttpPost]
-        public async Task Eject(string id)
+        public async Task<IActionResult> Eject(string id)
         {
             await AtomicActions.performActionAsync(
                 Player.NewPlayer(id),
                 AtomicAction.Eject);
+
+            return await Display(id);
         }
 
         [HttpPost]
-        public async Task Forecast(string id)
+        public async Task<IActionResult> Forecast(string id)
         {
             await AtomicActions.performActionAsync(
                 Player.NewPlayer(id),
                 AtomicAction.Forecast);
+
+            return await Display(id);
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Display(string id)
+        {
+            await Task.Delay(250);
+
+            var display = LyrionKnownPlayers.known.IsEmpty
+                ? new("Radio Home Engine", "No Squeezebox players connected")
+                : await Players.getDisplayNowAsync(Player.NewPlayer(id));
+
+            return View(display);
         }
     }
 }

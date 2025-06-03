33131e49ce0fcd6ff9fb8a9493d525b61327868c
Isaac Schemm <isaacschemm@gmail.com>
2025-06-02 20:58:49 -0500

    Move channels list

---
+++ RadioHomeEngine.AspNetCore/Controllers/ChannelsController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using RadioHomeEngine.AspNetCore.Models;
+
+namespace RadioHomeEngine.AspNetCore.Controllers
+{
+    public class ChannelsController : Controller
+    {
+        public async Task<IActionResult> Index(CancellationToken cancellationToken)
+        {
+            var allChannels = await ChannelListing.ListChannelsAsync(cancellationToken);
+
+            IReadOnlyList<PlayableChannelModel> channels = [
+                .. allChannels.Select(c => new PlayableChannelModel
+                {
+                    Category = c.category,
+                    Name = c.text,
+                    ImageSrc = c.icon,
+                    Url = c.url
+                })
+            ];
+
+            return View(channels);
+        }
+    }
+}
--- RadioHomeEngine.AspNetCore/Controllers/RadioController.cs
+++ RadioHomeEngine.AspNetCore/Controllers/RadioController.cs
@@ -7,23 +7,6 @@ namespace RadioHomeEngine.AspNetCore.Controllers
 {
     public class RadioController(IHttpClientFactory httpClientFactory) : Controller
     {
-        public async Task<IActionResult> Index(CancellationToken cancellationToken)
-        {
-            var allChannels = await ChannelListing.ListChannelsAsync(cancellationToken);
-
-            IReadOnlyList<PlayableChannelModel> channels = [
-                .. allChannels.Select(c => new PlayableChannelModel
-                {
-                    Category = c.category,
-                    Name = c.text,
-                    ImageSrc = c.icon,
-                    Url = c.url
-                })
-            ];
-
-            return View(channels);
-        }
-
         public IActionResult SiriusXM()
         {
             return View();
